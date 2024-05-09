5a580557bb6008993999b2f555d6bca7dffb69c8
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-05-09 15:58:18 +0530

    Update fetch-latest-version.py

--- tests/sources/fetch-latest-version.py
+++ tests/sources/fetch-latest-version.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 def get_latest_python_version():
     url = "https://www.python.org/downloads/"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     version = soup.find('div', {'class': 'download-unknown'}).find('span').text.split(' ')[1]
-    print(version)
+    return version
 
 if __name__ == "__main__":
-    get_latest_python_version()
+    version = get_latest_python_version()
+    print(f"::set-output name=version::{version}")
