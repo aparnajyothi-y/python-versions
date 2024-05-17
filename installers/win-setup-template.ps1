2e05faf74310904667b065a63fa092bb2b652c4b
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-05-17 12:03:57 +0530

    Update win-setup-template.ps1

--- installers/win-setup-template.ps1
+++ installers/win-setup-template.ps1
@@ -133,6 +133,7 @@ Copy-Item -Path ./$PythonExecName -Destination $PythonArchPath | Out-Null
 Write-Host "Install Python $Version in $PythonArchPath..."
 $ExecParams = Get-ExecParams -InstallerType $InstallerType -PythonArchPath $PythonArchPath
 cmd.exe /c "cd $PythonArchPath && call $PythonExecName $ExecParams /quiet"
+Write-Host "After install Python $Version in $PythonArchPath...  cmd.exe /c "cd $PythonArchPath && call $PythonExecName $ExecParams /quiet""
 if ($LASTEXITCODE -ne 0) {
     Throw "Error happened during Python installation"
 }
