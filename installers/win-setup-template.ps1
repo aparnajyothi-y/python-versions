f0a40d35b642eaa17aa93e42dec877420af133f7
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-06-10 17:13:39 +0530

    Update win-setup-template.ps1

--- installers/win-setup-template.ps1
+++ installers/win-setup-template.ps1
@@ -176,7 +176,7 @@ if ($MajorVersion -ne "2") {
 
 Write-Host "Install and upgrade Pip"
 $Env:PIP_ROOT_USER_ACTION = "ignore"
-$PythonExePath = Join-Path -Path $PythonArchPath -ChildPath $PythonExecName
+$PythonExePath = Join-Path -Path $PythonArchPath -ChildPath "python.exe"
 Write-Host "Install and upgrade Pip : $PythonExePath"
 cmd.exe /c "$PythonExePath -m ensurepip"
 cmd.exe /c "$PythonExePath -m pip install --upgrade --force-reinstall pip --no-warn-script-location"
