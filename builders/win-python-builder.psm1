e9b5e783bd8b9f59775d50da93148f68cbd58e5a
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-06-14 10:36:48 +0530

    Update win-python-builder.psm1

--- builders/win-python-builder.psm1
+++ builders/win-python-builder.psm1
@@ -60,7 +60,10 @@ class WinPythonBuilder : PythonBuilder {
             } else {
                 $ArchitectureExtension = ".amd64"
             }
+        } else ($this.Architecture -eq "arm64") {
+                $ArchitectureExtension = "-arm64"
         }
+
 
         return $ArchitectureExtension
     }
